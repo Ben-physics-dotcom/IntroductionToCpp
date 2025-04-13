2a3513b1835a2c292de9376e0ba69f95390d57ac
Ben Fliegener <ben.j.fliegener1999@gmail.com>
2025-04-13 15:49:28 +0200

    Add Input and Variables example programs

--- Lectures/Introduction/HelloWorld.cpp
+++ Lectures/Introduction/HelloWorld.cpp
@@ -2,6 +2,6 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hello, World!" << endl; // print hello world
+    cout << "Hello, World!\n" << endl; // print hello world
     return 0; // return 0 to indicate success
 }
