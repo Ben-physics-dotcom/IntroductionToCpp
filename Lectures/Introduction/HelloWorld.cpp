2bf51e5a5665bb5f92338cf21414ed07de4508a4
Ben Fliegener <ben.j.fliegener1999@gmail.com>
2025-04-13 15:36:22 +0200

    Add HelloWorld.cpp example program

---
+++ Lectures/Introduction/HelloWorld.cpp
@@ -0,0 +1,7 @@
+// a hello world program
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl; // print hello world
+    return 0; // return 0 to indicate success
+}
